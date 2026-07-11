f10bffcbfb458241b9f150670ae7577d3e35ea68
ElyseB725 <elise_badr@hotmail.com>
2026-07-11 18:34:56 +0300

    Initial commit

---
+++ src/test/java/com/claude/test/claudeproject/ClaudeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claude.test.claudeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
